edba5b8e7c6a4b0ab5e525b34603219477e0e6f5
jimmyfrysinger <jimmyfrysinger@gmail.com>
2024-07-07 13:16:51 -0400

    Install Tailwind, Components added

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import logo from "../assets/images/logo.svg";
+
+const Header = () => {
+  return (
+    <header className="">
+      <img src={logo} alt="URBN User Lookup" />
+    </header>
+  );
+};
+
+export default Header;
